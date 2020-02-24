97663b4af843ba68bbd839fdac5413e17e74acc5
akshayakky <akshaysmpt01@gmail.com>
2020-02-24 12:10:25 +0530

    ADDED : Resetting The Board With Empty Values

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome to tic-tac-toe game"
+
+#CONSTANTS
+NO_OF_ROWS=3
+NO_OF_COLUMNS=3
+
+for (( row=0; row<$NO_OF_ROWS; row++ ))
+do
+	for (( column=0; column<$NO_OF_COLUMNS; column++ ))
+	do
+		board[$row,$column]=""
+	done
+done
