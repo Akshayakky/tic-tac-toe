d8723d250dc98b7afc05eb7fa638d73e4f445afd
akshayakky <akshaysmpt01@gmail.com>
2020-02-24 12:26:32 +0530

    ADDED : Assigning Letter To Player

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -5,11 +5,34 @@ echo "Welcome to tic-tac-toe game"
 #CONSTANTS
 NO_OF_ROWS=3
 NO_OF_COLUMNS=3
+O=0
+X=1
 
-for (( row=0; row<$NO_OF_ROWS; row++ ))
-do
-	for (( column=0; column<$NO_OF_COLUMNS; column++ ))
+#VARIABLES
+letter=$((RANDOM%2))
+
+function resetBoard(){
+	for (( row=0; row<$NO_OF_ROWS; row++ ))
 	do
-		board[$row,$column]=""
+		for (( column=0; column<$NO_OF_COLUMNS; column++ ))
+		do
+			board[$row,$column]=""
+		done
 	done
-done
+}
+
+function assignLetter(){
+	if [ $letter -eq $X ]
+	then
+		computer="O"
+		player="X"
+	else
+		computer="X"
+		player="O"
+	fi
+	echo "Player : $player"
+	echo "Computer : $computer"
+}
+
+resetBoard
+assignLetter
